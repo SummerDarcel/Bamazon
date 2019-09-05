860515d7d1235b8aab8405f125533515bce82d80
Summer Stevenson <50718811+SummerDarcel@users.noreply.github.com>
2019-09-05 13:36:53 -0700

    Update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,12 @@
+var mysql = require("mysql2");
+var inquirer = require('inquirer');
+
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "mysqlworkbench",
+  database: "bamazon"
+});
 
