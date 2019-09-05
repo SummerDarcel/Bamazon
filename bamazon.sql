b3e437bfa56ab28edb9baee63d2f8665247f5c15
Summer Stevenson <50718811+SummerDarcel@users.noreply.github.com>
2019-09-05 13:00:31 -0700

    Update bamazon.sql

--- bamazon.sql
+++ bamazon.sql
@@ -10,4 +10,14 @@ CREATE TABLE products (
     stock_quantity INTEGER(10) NOT NULL
 );
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES 
+VALUES
+("Living Dead Doll The Exorcist Regan", "Collectables", 99.00, 2),
+("Halloween 1978 Movie Poster 27 x 40", "Home Decor", 18.98, 100),
+("Hellraiser Collection Blu-ray" , "Movies/TV", 25.98, 200),
+("Sandman 10 Volume Slipcase Set", "Books", 335.84, 1),
+("Vintage Wine Estates Game of Thrones 3 Bottle Wine Set", "Wine", 65, 100),
+("Pee Wee Herman T-Shirt", "Apparel", 26, 300),
+("The John Wick Sixth Scale Collectible Figure","Collectables", 243, 3),
+("
+
+
