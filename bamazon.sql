92950a783446dcfec3836d288672d699b3c32a77
Summer Stevenson <50718811+SummerDarcel@users.noreply.github.com>
2019-09-05 11:58:07 -0700

    Update bamazon.sql

--- bamazon.sql
+++ bamazon.sql
@@ -1,1 +1,13 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+USE bamazon_db;
 
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(50) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INTEGER(10) NOT NULL
+);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
