30c5a79e4044a0cbbe1b72114cf8ac36e032b331
Summer Stevenson <50718811+SummerDarcel@users.noreply.github.com>
2019-09-05 13:28:58 -0700

    Update bamazon.sql

--- bamazon.sql
+++ bamazon.sql
@@ -5,10 +5,11 @@ USE bamazon_db;
 CREATE TABLE products (
     item_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(100) NOT NULL,
-    department_name VARCHAR(50) NOT NULL,
+    department_name VARCHAR(25) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     stock_quantity INTEGER(10) NOT NULL
 );
+
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES
 ("Living Dead Doll The Exorcist Regan", "Collectables", 99.00, 2),
@@ -18,6 +19,11 @@ VALUES
 ("Vintage Wine Estates Game of Thrones 3 Bottle Wine Set", "Wine", 65, 100),
 ("Pee Wee Herman T-Shirt", "Apparel", 26, 300),
 ("The John Wick Sixth Scale Collectible Figure","Collectables", 243, 3),
-("
+("Spaced: The Complete Series", "Movies/TV", 29.98, 100),
+("Arwen Evenstar Pendant - Lord of The Rings", "Jewelry", 149, 20),
+("3D Skull Ice Mold Silicone Tray", "Kitchen", 14.95, 70),
+("Horror House Shower Curtain", "Home Decor", 19.95, 30),
+
+
 
 
