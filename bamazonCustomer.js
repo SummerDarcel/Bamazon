a3b566dd980d9cf7d40ee758c6f91de7907f63d5
Summer Stevenson <50718811+SummerDarcel@users.noreply.github.com>
2019-09-05 17:51:20 -0700

    Update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,3 +10,20 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
+//Display all of the items available for sale
+
+//Prompt users with two messages
+//The first should ask them the ID of the product they would like to buy.
+//The second message should ask how many units of the product they would like to buy.
+
+//Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+//If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
+
+//
+
+
+
+
+
+
